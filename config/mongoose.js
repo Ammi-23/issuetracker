cdb68e0482f3fcf1d940befb5971495f2dec734e
Ammi <ammipratima@gmail.com>
2023-08-17 00:03:22 +0530

    updated db

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose= require('mongoose');
 //connect to the database
 mongoose.connect('mongodb+srv://ammipratima:Lock&key23@issuetracker.ycnmbwy.mongodb.net/?retryWrites=true&w=majority');
-const db = mongoose.connection;
+let db = mongoose.connection;
 
 //if error in connecting db
 db.on('error', console.error.bind(console, "Error connecting to mongodb"));
